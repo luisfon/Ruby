3319fea171ebfebcb6da00743dcb682730ee6967
luisfon <ramirezfonsecaluisantonio@hotmail.com>
2016-08-30 10:21:26 -0500

    declaración de variables

---
+++ variables.rb
@@ -0,0 +1,31 @@
+class Variables
+
+  def initialize()
+
+  end
+
+ def var()
+
+   #declaramos una variable
+   nombre="luis antonio"
+
+   #imprimimos la variable
+
+   puts nombre
+
+   #declaramos numeros
+
+   num1=4
+   num2=45
+
+   resultado = num1+num2
+
+   puts resultado
+
+
+ end
+
+end
+
+obj=Variables.new()
+obj.var()
