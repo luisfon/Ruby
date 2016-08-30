86faa2c6426148612deae11ca7b6da0e8e3d31f8
luisfon <ramirezfonsecaluisantonio@hotmail.com>
2016-08-30 02:02:12 -0500

    comentarios en ruby

---
+++ comentarios.rb
@@ -0,0 +1,25 @@
+class Comentarios
+
+=begin
+comentario de varias lineas
+=end
+
+    def initialize()
+
+    end
+
+    def Saludo()
+
+      puts "hola mundo" #imprimo el mensaje
+
+    end
+
+
+#se crea el objeto
+  obj=Comentarios.new()
+
+#llamamos el el metodo mediante el obj
+  obj.Saludo()
+
+
+end
