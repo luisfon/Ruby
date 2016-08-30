9eb1a30eded9e01c9fa0940028f2029c4711bb91
luisfon <ramirezfonsecaluisantonio@hotmail.com>
2016-08-30 01:53:23 -0500

    hola mundo

---
+++ hola_mundo.rb
@@ -0,0 +1,19 @@
+class HolaMundo
+
+def initialize()
+
+end
+
+def Saludo()
+
+  puts "hola mundo"
+
+end
+
+end
+
+#se crea el objeto
+obj=HolaMundo.new()
+
+#llamamos el el metodo mediante el obj
+obj.Saludo()
